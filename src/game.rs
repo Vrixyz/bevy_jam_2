5715b129aca8c488d9850dba10bf42e547a81023
Thierry Berger <contact@thierryberger.com>
2022-08-25 02:33:53 +0200

    gameplay loop

---
+++ src/game.rs
@@ -0,0 +1,406 @@
+use crate::{cursor::MousePos, particles::ParticleExplosion, GameState, TextFont};
+use bevy::{
+    input::{mouse::MouseButtonInput, ButtonState},
+    math::Vec3Swizzles,
+    prelude::*,
+};
+use rand::Rng;
+
+pub struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system_set(SystemSet::on_enter(GameState::Game).with_system(new_game))
+            .add_system_set(
+                SystemSet::on_update(GameState::Game)
+                    .with_system(update_inventory)
+                    .with_system(handle_clicks)
+                    .with_system(react_play_round)
+                    .with_system(visibility_selection),
+            )
+            .add_system_set(SystemSet::on_exit(GameState::Game).with_system(despawn_game));
+    }
+}
+
+#[derive(Component, Debug, PartialEq, Clone)]
+enum Operation {
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    // TODO: modulo, sqrt, power...
+}
+
+impl std::fmt::Display for Operation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Operation::Plus => "+",
+                Operation::Minus => "-",
+                Operation::Multiply => "*",
+                Operation::Divide => "/",
+            }
+        )
+    }
+}
+
+impl Operation {
+    fn apply(&self, n1: f32, n2: f32) -> Result<f32, ()> {
+        match self {
+            Operation::Plus => Ok(n1 + n2),
+            Operation::Minus => Ok(n1 - n2),
+            Operation::Multiply => Ok(n1 * n2),
+            Operation::Divide => {
+                if n2 == 0f32 {
+                    Err(())
+                } else {
+                    Ok(n1 / n2)
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct PlayingNumber {
+    pub entity: Entity,
+    pub inventory_index: usize,
+}
+
+#[derive(Debug, Clone)]
+struct PlayRound {
+    pub operation: Option<Operation>,
+    pub number1: Option<PlayingNumber>,
+    pub number2: Option<PlayingNumber>,
+}
+
+impl PlayRound {
+    pub fn reset(&mut self) {
+        self.operation = None;
+        self.number1 = None;
+        self.number2 = None;
+    }
+}
+
+struct TargetNumber {
+    pub target: f32,
+}
+
+struct Inventory {
+    pub numbers: Vec<f32>,
+}
+
+#[derive(Component)]
+struct InventorySlot {
+    inventory_index: usize,
+}
+
+#[derive(Component)]
+struct SelectionVisual(pub Entity);
+
+#[derive(Component)]
+struct GameEntity;
+
+fn despawn_game(
+    mut commands: Commands,
+    q_to_despawn: Query<Entity, Or<(With<GameEntity>, With<InventorySlot>, With<Operation>)>>,
+) {
+    for e in q_to_despawn.iter() {
+        commands.entity(e).despawn_recursive();
+    }
+}
+
+fn new_game(mut commands: Commands, font: Res<TextFont>) {
+    // TODO: use a cross platform rand
+    let mut rand = rand::thread_rng();
+    let mut numbers = vec![];
+    for _ in 0..10 {
+        numbers.push(rand.gen_range(0..=10));
+    }
+    commands.insert_resource(Inventory {
+        numbers: numbers.iter().map(|n| *n as f32).collect(),
+    });
+
+    // TODO: simulate operations + end up on a doable target
+    let target = rand.gen_range(-1000..=1000);
+    commands.insert_resource(TargetNumber {
+        target: target as f32,
+    });
+    commands.insert_resource(PlayRound {
+        operation: None,
+        number1: None,
+        number2: None,
+    });
+
+    let text_alignment = TextAlignment::CENTER;
+    let text_style = TextStyle {
+        font: font.0.clone(),
+        font_size: 60.0,
+        color: Color::WHITE,
+    };
+    commands
+        .spawn_bundle(Text2dBundle {
+            text: Text::from_section(format!("Target: {target}"), text_style.clone())
+                .with_alignment(text_alignment),
+            transform: Transform::from_translation(Vec3::new(0f32, 300f32, 20f32)),
+            ..default()
+        })
+        .insert(GameEntity);
+
+    let operations = vec![
+        Operation::Plus,
+        Operation::Minus,
+        Operation::Multiply,
+        Operation::Divide,
+    ];
+    let spacing = 100f32;
+    let offset = Vec2::new(-((operations.len() - 1) as f32 * spacing) / 2f32, -50f32);
+    for (x, op) in operations.iter().enumerate() {
+        let mut op_commands = commands.spawn_bundle(Text2dBundle {
+            text: Text::from_section(format!("{op}"), text_style.clone())
+                .with_alignment(text_alignment),
+            transform: Transform::from_translation(
+                (Vec2::new(x as f32 * spacing, 0f32) + offset).extend(20f32),
+            ),
+            ..default()
+        });
+        op_commands.insert(op.clone());
+
+        let mut visual_entity = None;
+        op_commands.with_children(|parent| {
+            visual_entity = Some(
+                parent
+                    .spawn_bundle(SpriteBundle {
+                        sprite: Sprite {
+                            color: Color::YELLOW_GREEN,
+                            custom_size: Some(Vec2::splat(50f32)),
+                            ..default()
+                        },
+                        transform: Transform::from_translation(Vec2::ZERO.extend(-1f32)),
+                        visibility: Visibility { is_visible: false },
+                        ..default()
+                    })
+                    .id(),
+            );
+        });
+
+        let op_entity = op_commands.id();
+        commands
+            .entity(op_entity)
+            .insert(SelectionVisual(visual_entity.unwrap()));
+    }
+}
+
+fn update_inventory(
+    mut commands: Commands,
+    font: Res<TextFont>,
+    inventory: Res<Inventory>,
+    q_inventory_slots: Query<Entity, With<InventorySlot>>,
+) {
+    if inventory.is_changed() {
+        for e in q_inventory_slots.iter() {
+            commands.entity(e).despawn_recursive();
+        }
+        let text_alignment = TextAlignment::CENTER;
+        let text_style = TextStyle {
+            font: font.0.clone(),
+            font_size: 60.0,
+            color: Color::WHITE,
+        };
+        let column_count = 5;
+        let spacing = 100f32;
+        let offset = Vec2::new(-((column_count - 1) as f32 * spacing) / 2f32, 200f32);
+        for (i, number) in inventory.numbers.iter().enumerate() {
+            let x = i % column_count;
+            let y = i / column_count;
+            let mut slot = commands.spawn_bundle(Text2dBundle {
+                text: Text::from_section(
+                    format!("{number:.1}").trim_end_matches(".0"),
+                    text_style.clone(),
+                )
+                .with_alignment(text_alignment),
+                transform: Transform::from_translation(
+                    (Vec2::new(x as f32 * spacing, -(y as f32) * spacing) + offset).extend(20f32),
+                ),
+                ..default()
+            });
+
+            slot.insert(InventorySlot { inventory_index: i });
+            let slot_entity = slot.id();
+            let mut visual_entity = None;
+            slot.with_children(|parent| {
+                visual_entity = Some(
+                    parent
+                        .spawn_bundle(SpriteBundle {
+                            sprite: Sprite {
+                                color: Color::YELLOW_GREEN,
+                                custom_size: Some(Vec2::splat(50f32)),
+                                ..default()
+                            },
+                            transform: Transform::from_translation(Vec2::ZERO.extend(-1f32)),
+                            visibility: Visibility { is_visible: false },
+                            ..default()
+                        })
+                        .id(),
+                );
+            });
+            commands
+                .entity(slot_entity)
+                .insert(SelectionVisual(visual_entity.unwrap()));
+        }
+    }
+}
+
+fn handle_clicks(
+    mut mouse_button_input_events: EventReader<MouseButtonInput>,
+    mouse_pos: Res<MousePos>,
+    mut play_round: ResMut<PlayRound>,
+    q_inventory_slots: Query<(Entity, &Transform, &InventorySlot)>,
+    q_operations: Query<(Entity, &Transform, &Operation)>,
+) {
+    for event in mouse_button_input_events.iter() {
+        if ButtonState::Pressed == event.state {
+            let mut found_something = false;
+            for (e, t, slot) in &q_inventory_slots {
+                let distance = t.translation.xy().distance(mouse_pos.0);
+                if distance < 50f32 {
+                    if let Some(n1) = &play_round.number1 {
+                        if n1.entity == e {
+                            play_round.number1 = None;
+                            found_something = true;
+                            break;
+                        }
+                    }
+                    if let Some(n2) = &play_round.number2 {
+                        if n2.entity == e {
+                            play_round.number2 = None;
+                            found_something = true;
+                            break;
+                        }
+                    }
+                    if play_round.number1.is_none() {
+                        play_round.number1 = Some(PlayingNumber {
+                            entity: e,
+                            inventory_index: slot.inventory_index,
+                        });
+                        found_something = true;
+                        break;
+                    }
+                    if play_round.number2.is_none() {
+                        play_round.number2 = Some(PlayingNumber {
+                            entity: e,
+                            inventory_index: slot.inventory_index,
+                        });
+                        found_something = true;
+                        break;
+                    }
+                    // close to something, but we cannot select more.
+                    // TODO: show feedback to encourage deselection
+                    break;
+                }
+            }
+            if found_something {
+                continue;
+            }
+            for (e, t, operation) in &q_operations {
+                let distance = t.translation.xy().distance(mouse_pos.0);
+                if distance < 50f32 {
+                    if let Some(op) = &play_round.operation {
+                        if op == operation {
+                            play_round.operation = None;
+                            break;
+                        }
+                    }
+                    if play_round.operation.is_none() {
+                        play_round.operation = Some(operation.clone());
+                        break;
+                    }
+                    // close to something, but we cannot select more.
+                    // TODO: show feedback to encourage deselection
+                    break;
+                }
+            }
+        }
+    }
+}
+
+fn visibility_selection(
+    mut play_round: ResMut<PlayRound>,
+    q_selectable: Query<(Entity, &SelectionVisual, Option<&Operation>)>,
+    mut q_visibility: Query<(&mut Visibility, &mut Sprite)>,
+) {
+    if play_round.is_changed() {
+        for (e, v, op) in q_selectable.iter() {
+            if let Some(n1) = &play_round.number1 {
+                if n1.entity == e {
+                    let mut res = q_visibility.get_mut(v.0).unwrap();
+                    res.0.is_visible = true;
+                    res.1.color = Color::GREEN;
+                    continue;
+                }
+            }
+            if let Some(n2) = &play_round.number2 {
+                if n2.entity == e {
+                    let mut res = q_visibility.get_mut(v.0).unwrap();
+                    res.0.is_visible = true;
+                    res.1.color = Color::BLUE;
+                    continue;
+                }
+            }
+            if let Some(op) = op {
+                if let Some(selected_op) = &play_round.operation {
+                    if selected_op == op {
+                        let mut res = q_visibility.get_mut(v.0).unwrap();
+                        res.0.is_visible = true;
+                        res.1.color = Color::YELLOW_GREEN;
+                        continue;
+                    }
+                }
+            }
+            q_visibility.get_mut(v.0).unwrap().0.is_visible = false;
+        }
+    }
+}
+
+fn react_play_round(
+    mut mouse_button_input_events: EventReader<MouseButtonInput>,
+    mut particles: EventWriter<ParticleExplosion>,
+    mut inventory: ResMut<Inventory>,
+    mut play_round: ResMut<PlayRound>,
+    mut state: ResMut<State<GameState>>,
+) {
+    if play_round.is_changed() {
+        if let PlayRound {
+            operation: Some(op),
+            number1: Some(n1),
+            number2: Some(n2),
+        } = play_round.clone()
+        {
+            let result = op.apply(
+                inventory.numbers[n1.inventory_index],
+                inventory.numbers[n2.inventory_index],
+            );
+            if let Ok(result) = result {
+                play_round.as_mut().reset();
+                inventory.numbers[n1.inventory_index] = result;
+                inventory.numbers.remove(n2.inventory_index);
+                particles.send(ParticleExplosion {
+                    location: Vec2::ZERO,
+                    color: Color::ANTIQUE_WHITE,
+                });
+                if inventory.numbers.len() == 1 {
+                    // TODO: Delay, then show "done" screen
+                    let _ = state.set(GameState::Menu);
+                }
+            } else {
+                play_round.as_mut().reset();
+                particles.send(ParticleExplosion {
+                    location: Vec2::ZERO,
+                    color: Color::ORANGE_RED,
+                })
+            }
+        }
+    }
+}
---
+++ src/menu.rs
@@ -0,0 +1,226 @@
+use bevy::prelude::*;
+use bevy_jornet::Leaderboard;
+
+use crate::{GameState, TextFont};
+pub struct MenuPlugin;
+
+const BACKGROUND: &str = "439775";
+const BUTTON: &str = "2A4747";
+const TEXT: &str = "8ecae6";
+
+#[derive(Component)]
+struct MenuUI;
+
+impl Plugin for MenuPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system_set(SystemSet::on_enter(GameState::Menu).with_system(display_menu))
+            .add_system_set(
+                SystemSet::on_update(GameState::Menu)
+                    .with_system(button_system)
+                    .with_system(display_scores),
+            )
+            .add_system_set(SystemSet::on_exit(GameState::Menu).with_system(despawn_menu));
+    }
+}
+
+fn display_menu(mut commands: Commands, font: Res<TextFont>, leaderboard: Res<Leaderboard>) {
+    commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                margin: UiRect::all(Val::Auto),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                flex_direction: FlexDirection::ColumnReverse,
+                border: UiRect::all(Val::Px(30.0)),
+                ..default()
+            },
+            color: Color::hex(BACKGROUND).unwrap().into(),
+            ..default()
+        })
+        .insert(MenuUI)
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle::from_section(
+                "Math it",
+                TextStyle {
+                    font: font.0.clone(),
+                    font_size: 60.0,
+                    color: Color::hex(TEXT).unwrap(),
+                },
+            ));
+            parent
+                .spawn_bundle(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Row,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        margin: UiRect::all(Val::Px(20.0)),
+                        ..default()
+                    },
+                    color: Color::NONE.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent
+                        .spawn_bundle(NodeBundle {
+                            style: Style {
+                                size: Size::new(Val::Px(300.0), Val::Undefined),
+                                flex_direction: FlexDirection::ColumnReverse,
+                                justify_content: JustifyContent::Center,
+                                align_items: AlignItems::Center,
+                                margin: UiRect::all(Val::Px(20.0)),
+                                ..default()
+                            },
+                            color: Color::NONE.into(),
+                            ..default()
+                        })
+                        .insert(LeaderboardMarker::Player);
+                    parent
+                        .spawn_bundle(NodeBundle {
+                            style: Style {
+                                size: Size::new(Val::Px(150.0), Val::Undefined),
+                                flex_direction: FlexDirection::ColumnReverse,
+                                justify_content: JustifyContent::Center,
+                                align_items: AlignItems::Center,
+                                margin: UiRect::all(Val::Px(20.0)),
+                                ..default()
+                            },
+                            color: Color::NONE.into(),
+                            ..default()
+                        })
+                        .insert(LeaderboardMarker::Score);
+                });
+
+            parent
+                .spawn_bundle(ButtonBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    color: Color::hex(BUTTON).unwrap().into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn_bundle(TextBundle::from_section(
+                        "Play",
+                        TextStyle {
+                            font: font.0.clone(),
+                            font_size: 40.0,
+                            color: Color::hex(TEXT).unwrap(),
+                        },
+                    ));
+                });
+        });
+    commands
+        .spawn_bundle(
+            TextBundle::from_sections([
+                TextSection {
+                    value: "you are: ".to_string(),
+                    style: TextStyle {
+                        font: font.0.clone(),
+                        font_size: 20.0,
+                        color: Color::hex(TEXT).unwrap(),
+                    },
+                },
+                TextSection {
+                    value: leaderboard
+                        .get_player()
+                        .map(|p| p.name.clone())
+                        .unwrap_or_default(),
+                    style: TextStyle {
+                        font: font.0.clone(),
+                        font_size: 25.0,
+                        color: Color::hex(TEXT).unwrap(),
+                    },
+                },
+            ])
+            .with_style(Style {
+                position_type: PositionType::Absolute,
+                position: UiRect {
+                    left: Val::Px(10.0),
+                    bottom: Val::Px(10.0),
+                    ..default()
+                },
+                ..default()
+            }),
+        )
+        .insert(MenuUI)
+        .insert(PlayerName);
+
+    leaderboard.refresh_leaderboard();
+}
+
+#[derive(Component)]
+struct PlayerName;
+
+#[derive(Component)]
+enum LeaderboardMarker {
+    Score,
+    Player,
+}
+
+fn display_scores(
+    leaderboard: Res<Leaderboard>,
+    mut commands: Commands,
+    font: Res<TextFont>,
+    root_ui: Query<(Entity, &LeaderboardMarker)>,
+    mut player_name: Query<&mut Text, With<PlayerName>>,
+) {
+    if leaderboard.is_changed() {
+        if let Some(player) = leaderboard.get_player() {
+            player_name.single_mut().sections[1].value = player.name.clone();
+        }
+        let leaderboard = leaderboard.get_leaderboard();
+        for (root_entity, marker) in &root_ui {
+            commands.entity(root_entity).despawn_descendants();
+            for score in &leaderboard {
+                commands.entity(root_entity).with_children(|parent| {
+                    parent.spawn_bundle(TextBundle::from_section(
+                        match marker {
+                            LeaderboardMarker::Score => format!("{} ", score.score),
+                            LeaderboardMarker::Player => score.player.clone(),
+                        },
+                        TextStyle {
+                            font: font.0.clone(),
+                            font_size: 30.0,
+                            color: Color::hex(TEXT).unwrap(),
+                        },
+                    ));
+                });
+            }
+        }
+    }
+}
+
+fn despawn_menu(
+    mut commands: Commands,
+    root_ui: Query<Entity, (With<Node>, With<MenuUI>, Without<Parent>)>,
+) {
+    for entity in &root_ui {
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut state: ResMut<State<GameState>>,
+) {
+    for (interaction, mut color) in &mut interaction_query {
+        match *interaction {
+            Interaction::Clicked => {
+                *color = (Color::hex(BUTTON).unwrap() + Color::GRAY).into();
+                let _ = state.set(GameState::Game);
+            }
+            Interaction::Hovered => {
+                *color = (Color::hex(BUTTON).unwrap() + Color::DARK_GRAY).into();
+            }
+            Interaction::None => {
+                *color = Color::hex(BUTTON).unwrap().into();
+            }
+        }
+    }
+}
